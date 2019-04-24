7663d4275aaad1c41ec3f2b8e39c71ac0832b0ac
0thaer0 <44973413+0thaer0@users.noreply.github.com>
2019-04-24 19:32:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var prefix = "m!"
 //bc
 
 client.on("message", message => {
-    if (message.content.startsWith("m!obc")) {
+    if (message.content.startsWith("  ")) {
                  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join('m!');
